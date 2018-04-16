74cc92f1a2810a17748994a1958f26bad13953d6
Ahsan Khan <36322910+ahsan-khan1999@users.noreply.github.com>
2018-04-16 21:01:06 +0500

    Add files via upload

---
+++ Association.java
@@ -0,0 +1,11 @@
+public class Association{
+	public static void main(String[] args){
+	Teacher t1 = new Teacher();
+	//Student s1 =  new Student();
+	
+	System.out.println(t1.s1.getName());
+	t1.getName();
+	//s1.setId("ada");
+
+}
+}
